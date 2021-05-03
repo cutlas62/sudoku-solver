bf00698ebc8f75e0972cf7327daa732df6aef698
Carlos <carlos.santosr@ieee.org>
2021-05-03 19:42:33 -0400

    Added functions to read input and print the grid

--- main.cpp
+++ main.cpp
@@ -1,3 +1,47 @@
-int main(){
-	return 0;
+#include <cstdio>
+#include <iostream>
+#include <fstream>
+
+#include <string>
+
+using namespace std;
+
+char grid [9][9];
+
+void clearGrid (void) {
+    for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < 9; j++) {
+            grid[i][j] = 1;
+        }
+    }
+}
+
+void populateGrid (void) {
+    ifstream inFile("in/example.txt");
+    string tmp;
+    int i = 0;
+    while(getline(inFile, tmp)) {
+        for (int j = 0; j < 9; j++) {
+            grid[i][j] = tmp[j];
+        }
+        i++;
+    }
+    inFile.close();
+}
+
+void printGrid (void) {
+    for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < 9; j++) {
+            cout << grid[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    clearGrid();
+    populateGrid();
+    printGrid();
+
+    return 0;
 }

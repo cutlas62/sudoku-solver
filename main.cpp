faa135ffc43eacb70e3aa04fc0f6796e18e1d55c
Carlos <carlos.santosr@ieee.org>
2021-05-04 21:15:02 -0400

    Got it working using backtracking

--- main.cpp
+++ main.cpp
@@ -3,10 +3,11 @@
 #include <fstream>
 
 #include <string>
+#include <vector>
 
 using namespace std;
 
-uint8_t grid [9][9];
+vector<vector<uint8_t>> grid (9, vector<uint8_t>(9, 0));
 
 void clearGrid (void) {
     for (int i = 0; i < 9; i++) {
@@ -29,19 +30,80 @@ void populateGrid (void) {
     inFile.close();
 }
 
-void printGrid (void) {
+void printGrid (vector<vector<uint8_t>> &g) {
     for (int i = 0; i < 9; i++) {
         for (int j = 0; j < 9; j++) {
-            cout << (uint)grid[i][j] << " ";
+            cout << (uint)g[i][j] << " ";
         }
         cout << endl;
     }
 }
 
+bool validNumber (uint8_t curI, uint8_t curJ, uint8_t n) {
+    // Check vertical line
+    for (uint8_t j = 0; j < 9; j++) {
+        if (grid[curI][j] == n) {
+            return false;
+        }
+    }
+
+    // Check horizontal line
+    for (uint8_t i = 0; i < 9; i++) {
+        if (grid[i][curJ] == n) {
+            return false;
+        }
+    }
+
+    // Check local 3x3 square
+    uint8_t localI = curI / 3;
+    uint8_t localJ = curJ / 3;
+    for (uint8_t i = 0; i < 3; i++) {
+        for(uint8_t j = 0; j < 3; j++) {
+            if (grid[3 * localI + i][3 * localJ + j] == n) {
+                return false;
+            }
+        }
+    }
+
+    // If all the previous tests passed, the number is valid
+    return true;
+}
+
+uint32_t solveGrid (vector<vector<vector<uint8_t>>> &sol) {
+    for (uint8_t i = 0; i < 9; i++) {
+        for (uint8_t j = 0; j < 9; j++) {
+            if (grid[i][j] == 0) {
+                for (uint8_t n = 1; n <= 9; n++) {
+                    if (validNumber(i, j, n)) {
+                        grid[i][j] = n;
+                        solveGrid(sol);
+                        grid[i][j] = 0;
+                    }
+                }
+                return 0;
+            }
+        }
+    }
+
+    sol.push_back(grid);
+    return sol.size();
+}
+
 int main() {
     clearGrid();
     populateGrid();
-    printGrid();
+    printGrid(grid);
+
+    cout << endl << "Solving it..." << endl;
+
+    vector<vector<vector<uint8_t>>> solutions;
+    solveGrid(solutions);
+
+    cout << "Found " << solutions.size() << " solutions:" << endl;
+    for (vector<vector<uint8_t>> curGrid : solutions){
+    	printGrid(curGrid);
+    	cout << endl;
+    }
 
     return 0;
 }
